8fc1be0ae7e84d547858cef1a7d50045ab4f71da
kdheejb7 <39160667+kdheejb7@users.noreply.github.com>
2018-05-17 12:47:04 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> WEB </title>
+	</head>
+	<body>
+		<h1> WEB </h1>
+		<ol>
+			<li> html </li>
+		</ol>
+		<h2> welcome </h2>
+	</body>
+</html>
